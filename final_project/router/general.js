f9abaecf16280f37111f3da9876dc9e089dca28e
smhmr <116069860+smhmr@users.noreply.github.com>
2023-03-17 15:58:23 +0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,106 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+//************************ ASYNC FUNCTIONS *****************************
+const getBooksAsync = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000')
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getBooksDetailsByISBNAsync = async isbn => {
+  try {
+    const response = await axios.get(`http://localhost:5000/isbn/${isbn}`)
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getBooksDetailsByAuthorAsync = async author => {
+  try {
+    const response = await axios.get(`http://localhost:5000/author/${author}`)
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getBooksDetailsByTitleAsync = async title => {
+  try {
+    const response = await axios.get(`http://localhost:5000/title/${title}`)
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getBooksAsync();
+getBooksDetailsByISBNAsync(7);
+getBooksDetailsByAuthorAsync("Chinua Achebe")
+getBooksDetailsByTitleAsync("Fairy tales")
+
+
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username) {
+    return res.status(404).json({message: "Username not provided!"});    
+  }
+  if (!password) {
+    return res.status(404).json({message: "Password not provided!"});    
+  }
+  if (isValid(username)) { 
+    return res.status(409).json({message: "Username already exists!"});    
+  } 
+  users.push({"username":username, "password":password});
+  return res.status(200).json({message: "Username successfully registered."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));    
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  res.send(JSON.stringify(books[req.params.isbn], null, 4));
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author_books = [];
+  for (let key in books) {
+    if (books[key]["author"] == req.params.author) {
+      author_books.push(books[key]);
+    }
+  }
+  res.send(JSON.stringify(author_books, null, 4));  
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title_books = [];
+  for (let key in books) {
+    if (books[key]["title"] == req.params.title) { 
+      title_books.push(books[key]);
+    }
+  }
+  res.send(JSON.stringify(title_books, null, 4)); 
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books[req.params.isbn]["reviews"], null, 4)); 
 });
+
 
 module.exports.general = public_users;
